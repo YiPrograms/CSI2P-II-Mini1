fc5248fd3bfb92b73e30984d9cab44d5598596b6
Yi Kuo <kuokuoyiyi@gmail.com>
2020-11-18 15:50:06 +0800

    Submit

--- mini1.c
+++ mini1.c
@@ -9,22 +9,50 @@ For the language grammar, please refer to Grammar section on the github page:
 */
 
 #define MAX_LENGTH 200
+#define NULLREG -300
+
 typedef enum {
-	ASSIGN, ADD, SUB, MUL, DIV, REM, PREINC, PREDEC, POSTINC, POSTDEC, IDENTIFIER, CONSTANT, LPAR, RPAR, PLUS, MINUS
+	ASSIGN, ADD, SUB, MUL, DIV, REM, PREINC, PREDEC, POSTINC, POSTDEC, IDENTIFIER, CONSTANT, LPAR, RPAR, PLUS, MINUS, LOAD, STORE
 } Kind;
+
 typedef enum {
 	STMT, EXPR, ASSIGN_EXPR, ADD_EXPR, MUL_EXPR, UNARY_EXPR, POSTFIX_EXPR, PRI_EXPR
 } GrammarState;
+
+typedef enum {
+	EMPTY, REGISTER, VALUE, ADDRESS
+} OperandType;
+
+typedef enum {
+	FREE, USED, IDEN
+} RegisterState;
+
 typedef struct TokenUnit {
 	Kind kind;
 	int val; // record the integer value or variable name
 	struct TokenUnit *next;
 } Token;
+
 typedef struct ASTUnit {
 	Kind kind;
 	int val; // record the integer value or variable name
 	struct ASTUnit *lhs, *mid, *rhs;
 } AST;
+
+typedef struct ISAUnit {
+	Kind ins; // Reuse Kind enum
+	OperandType o1_type;
+	unsigned int o1; // Register from 1 to 256
+	OperandType o2_type;
+	unsigned int o2;
+	OperandType o3_type;
+	unsigned int o3;
+	int redundant;
+	int deleted;
+	int first_run, last_res, is_const;
+} ISA;
+
+
 
 /// utility interfaces
 
@@ -38,7 +66,7 @@ typedef struct ASTUnit {
 	exit(0);\
 }
 // You may set DEBUG=1 to debug. Remember setting back to 0 before submit.
-#define DEBUG 1
+int DEBUG = 0;
 // Split the input char array into token linked list.
 Token *lexer(const char *in);
 // Create a new token.
@@ -67,6 +95,20 @@ void semantic_check(AST *now);
 int codegen(AST *root);
 // Free the whole AST.
 void freeAST(AST *now);
+// Add ASM code
+void addISA(Kind ins, int o1, int o2, int o3);
+// Print single instruction
+void printISA(ISA *isa);
+// Find a not used register
+int findNewRegister();
+// Free used registers (not identifiers)
+void freeRegisters();
+// Store modified identifiers to memory
+void storeValues();
+// Print ASM code starting from s
+void printASM(int start);
+
+AST *simplifyTree(AST *root);
 
 /// debug interfaces
 
@@ -74,20 +116,53 @@ void freeAST(AST *now);
 void token_print(Token *in, size_t len);
 // Print AST tree.
 void AST_print(AST *head);
+// Print register status
+void printRegisters();
 
 char input[MAX_LENGTH];
 
-int main() {
+ISA code[MAX_LENGTH * 20];
+int code_len = 0;
+int last_len = 0;
+
+// Register state, 0: free, 1: used, 2: identifier
+// Stores register from 1 to 256, deduct 1 when printing
+int registers[260];
+// Registers of the three identifiers, 1 ~ 256
+int identifiers[3] = {-1, -1, -1};
+// Track modified registers
+int modified[3] = {0, 0, 0};
+
+int main(int argc, char **argv) {
+	if (argc == 2)
+		DEBUG = 1;
 	while (fgets(input, MAX_LENGTH, stdin) != NULL) {
 		Token *content = lexer(input);
 		size_t len = token_list_to_arr(&content);
+
+		if (len <= 0)
+			continue;
+
 		AST *ast_root = parser(content, len);
-		if (DEBUG) AST_print(ast_root);
+
 		semantic_check(ast_root);
+		ast_root = simplifyTree(ast_root);
+		if (DEBUG) AST_print(ast_root);
+
 		codegen(ast_root);
+		
+		freeRegisters();
+		if (DEBUG) {
+			printASM(last_len);
+			last_len = code_len;
+			printRegisters();
+		}
+
 		free(content);
 		freeAST(ast_root);
 	}
+	storeValues();
+	printASM(0);
 	return 0;
 }
 
@@ -162,7 +237,7 @@ size_t token_list_to_arr(Token **head) {
 	for (res = 0; now != NULL; res++) now = now->next;
 	now = (*head);
 	if (res != 0) (*head) = (Token*)malloc(sizeof(Token) * res);
-	for (int i = 0; i < res; i++) {
+	for (int i = 0; i < (int)res; i++) {
 		(*head)[i] = (*now);
 		del = now;
 		now = now->next;
@@ -172,7 +247,7 @@ size_t token_list_to_arr(Token **head) {
 }
 
 AST *parser(Token *arr, size_t len) {
-	for (int i = 1; i < len; i++) {
+	for (int i = 1; i < (int)len; i++) {
 		// correctly identify "ADD" and "SUB"
 		if (arr[i].kind == PLUS || arr[i].kind == MINUS) {
 			switch (arr[i - 1].kind) {
@@ -191,6 +266,9 @@ AST *parser(Token *arr, size_t len) {
 
 AST *parse(Token *arr, int l, int r, GrammarState S) {
 	AST *now = NULL;
+	if (l > r) {
+		err("Expected expression")
+	}
 	int nxt;
 	switch (S) {
 		case STMT:
@@ -227,13 +305,9 @@ AST *parse(Token *arr, int l, int r, GrammarState S) {
 		case UNARY_EXPR:
 			// TODO: Implement UNARY_EXPR.
 			// hint: Take POSTFIX_EXPR as reference.
-			if (arr[l].kind == PREINC || arr[l].kind == PREDEC || arr[l].kind == MINUS) {
+			if (arr[l].kind == PREINC || arr[l].kind == PREDEC || arr[l].kind == PLUS || arr[l].kind == MINUS) {
 				now = new_AST(arr[l].kind, 0);
 				now->mid = parse(arr, l + 1, r, UNARY_EXPR);
-				return now;
-			}
-			if (arr[l].kind == PLUS) {
-				now = parse(arr, l + 1, r, UNARY_EXPR);
 				return now;
 			}
 			return parse(arr, l, r, POSTFIX_EXPR);
@@ -247,7 +321,8 @@ AST *parse(Token *arr, int l, int r, GrammarState S) {
 			return parse(arr, l, r, PRI_EXPR);
 		case PRI_EXPR:
 			if (findNextSection(arr, l, r, condRPAR) == r) {
-				now = parse(arr, l + 1, r - 1, EXPR);
+				now = new_AST(LPAR, 0);
+				now->mid = parse(arr, l + 1, r - 1, EXPR);
 				return now;
 			}
 			if (l == r) {
@@ -298,9 +373,11 @@ int condRPAR(Kind kind) {
 
 void semantic_check(AST *now) {
 	if (now == NULL) return;
+	
 	// Left operand of '=' must be an identifier or identifier with one or more parentheses.
 	if (now->kind == ASSIGN) {
 		AST *tmp = now->lhs;
+		while (tmp->kind == LPAR) tmp = tmp->mid;
 		if (tmp->kind != IDENTIFIER)
 			err("Lvalue is required as left operand of assignment.");
 	}
@@ -311,7 +388,8 @@ void semantic_check(AST *now) {
 	if (now->kind == PREINC || now->kind == POSTINC ||
 		now->kind == PREDEC || now->kind == POSTDEC) {
 		AST *tmp = now->mid;
-		if (!(tmp->kind == IDENTIFIER || tmp->kind == ASSIGN))
+		while (tmp->kind == LPAR) tmp = tmp->mid;
+		if (tmp->kind != IDENTIFIER)
 			err("INC/DEC must follow an identifier.");
 	}
 
@@ -320,92 +398,277 @@ void semantic_check(AST *now) {
 	semantic_check(now->rhs);
 }
 
+AST *simplifyTree(AST *root) {
+	if (root == NULL)
+		return NULL;
 
-// Returns register 0~255
-int curReg = 0;
+	root->lhs = simplifyTree(root->lhs);
+	root->mid = simplifyTree(root->mid);
+	root->rhs = simplifyTree(root->rhs);
 
+	// Essential
+	if (root->kind == PLUS || root->kind == LPAR) {
+		if (root->mid == NULL) {
+			err("Expected expression")
+		}
+		AST *tmp = root;
+		root = root->mid;
+		free(tmp);
+		return root;
+	}
+	
+	return root;
+}
+
+int DONT_LOAD = 0;
+
+// Returns value if >0, register if <0 (-1 ~ -256) or NULLREG
 int codegen(AST *root) {
 	// TODO: Implement your codegen in your own way.
 	// You may modify the function parameter or the return type, even the whole structure as you wish.
-	if (root == NULL)
-		return -1;
 
-	int l, r;
+	if (root == NULL)
+		return NULLREG;
+
+	int l, r, dest;
 	switch (root->kind) {
 		case ASSIGN:
 			r = codegen(root->rhs);
-			printf("store [%d] r%d\n", 4 * (root->lhs->val - 'x'), r);
-			return r;
+			//printf("Rvalue: %d\n", r);
+			if (r >= 0 || registers[-r] == IDEN) {
+				dest = findNewRegister();
+				addISA(ADD, dest, 0, r);
+			} else {
+				dest = r;
+			}
+
+			DONT_LOAD = 1;
+			l = codegen(root->lhs);
+			DONT_LOAD = 0;
+			
+			if (l >= 0 || (l != NULLREG && registers[-l] != IDEN)) {
+				err("Lvalue should be an identifier!")
+			}
+
+			if (l != NULLREG)
+				registers[-l] = FREE;
+
+			identifiers[root->lhs->val - 'x'] = -dest;
+			modified[root->lhs->val - 'x'] = 1;
+
+			registers[-dest] = IDEN;
+			return dest;
 
 		case ADD:
-			l = codegen(root->lhs);
-			r = codegen(root->rhs);
-			printf("add r%d r%d r%d\n", l, l, r);
-			return l;
-
 		case SUB:
-			l = codegen(root->lhs);
-			r = codegen(root->rhs);
-			printf("sub r%d r%d r%d\n", l, l, r);
-			return l;
-
 		case MUL:
-			l = codegen(root->lhs);
-			r = codegen(root->rhs);
-			printf("mul r%d r%d r%d\n", l, l, r);
-			return l;
-
 		case DIV:
-			l = codegen(root->lhs);
-			r = codegen(root->rhs);
-			printf("div r%d r%d r%d\n", l, l, r);
-			return l;
 		case REM:
 			l = codegen(root->lhs);
 			r = codegen(root->rhs);
-			printf("rem r%d r%d r%d\n", l, l, r);
-			return l;
+
+			if (l < 0 && registers[-l] != IDEN)
+				dest = l;
+			else if (r < 0 && registers[-r] != IDEN)
+				dest = r;
+			else 
+				dest = findNewRegister();
+
+			addISA(root->kind, dest, l, r);
+
+			if (l < 0 && registers[-l] != IDEN)
+				registers[-l] = FREE;
+			if (r < 0 && registers[-r] != IDEN)
+				registers[-r] = FREE;
+			registers[-dest] = USED;
+			return dest;
 
 		case PREINC:
-			r = codegen(root->mid);
-			printf("add r%d r%d 1\n", r, r);
-			printf("store [%d] r%d\n", 4 * (root->mid->val - 'x'), r);
-			return r;
-
 		case PREDEC:
-			r = codegen(root->mid);
-			printf("sub r%d r%d 1\n", r, r);
-			printf("store [%d] r%d\n", 4 * (root->mid->val - 'x'), r);
-			return r;
-
 		case POSTINC:
-			r = codegen(root->mid);
-			printf("add r%d r%d 1\n", curReg, r);
-			printf("store [%d] r%d\n", 4 * (root->mid->val - 'x'), curReg);
-			return r;
-
 		case POSTDEC:
-			r = codegen(root->mid);
-			printf("add r%d r%d 1\n", curReg, r);
-			printf("store [%d] r%d\n", 4 * (root->mid->val - 'x'), curReg);
-			return r;
+			l = codegen(root->mid);
+
+			if (l >= 0 || registers[-l] != IDEN) {
+				err("INC/DEC should follow an identifier!")
+			}
+
+			if (root->kind == POSTINC || root->kind == POSTDEC) {
+				dest = findNewRegister();
+				registers[-dest] = USED;
+				addISA(ADD, dest, 0, l);
+			} else {
+				dest = l;
+			}
+
+			addISA((root->kind == PREINC || root->kind == POSTINC)? ADD: SUB, l, l, 1);
+			modified[root->mid->val - 'x'] = 1;
+			return dest;
 
 		case IDENTIFIER:
-			printf("load r%d [%d]\n", curReg, 4 * (root->val - 'x'));
-			return curReg++;
+			if (identifiers[root->val - 'x'] != -1)
+				return -identifiers[root->val - 'x'];
+			if (DONT_LOAD)
+				return NULLREG;
+			dest = findNewRegister();
+			registers[-dest] = IDEN;
+			identifiers[root->val - 'x'] = -dest;
+			addISA(LOAD, dest, 4 * (root->val - 'x'), 0);
+			// printf("Loaded %c into r%d\n", root->val, dest);
+			return dest;
 
 		case CONSTANT:
-			printf("add r%d 0 %d\n", curReg, root->val);
-			return curReg++;
+			return root->val;
 
 		case MINUS:
 			r = codegen(root->mid);
-			printf("sub r%d 0 r%d\n", r, r);
-			return r;
+			if (r >= 0 || registers[-r] == IDEN) {
+				dest = findNewRegister();
+				registers[-dest] = USED;
+			} else {
+				dest = r;
+			}
+
+			addISA(SUB, dest, 0, r);
+			return dest;
 
 		default:
 			err("AST kind not found!")
 	}
+	
+}
+
+// Returns (-1 ~ -256)
+int findNewRegister() {
+	for (int i = 1; i <= 256; i++)
+		if (registers[i] == FREE)
+			return -i;
+	
+	return NULLREG; // No registers left
+}
+
+void freeRegisters() {
+	for (int i = 1; i <= 256; i++)
+		if (registers[i] == USED)
+			registers[i] = FREE;
+}
+
+void addISA(Kind ins, int o1, int o2, int o3) {
+	ISA *new = &code[code_len++];
+	new->ins = ins;
+	if (ins == LOAD || ins == STORE) {
+		if (o1 < 0) {
+			new->o1_type = REGISTER;
+			new->o1 = -o1;
+		} else {
+			new->o1_type = ADDRESS;
+			new->o1 = o1;
+		}
+		if (o2 < 0) {
+			new->o2_type = REGISTER;
+			new->o2 = -o2;
+		} else {
+			new->o2_type = ADDRESS;
+			new->o2 = o2;
+		}
+		new->o3_type = EMPTY;
+		new->o3 = 0;
+	} else {
+		if (o1 < 0) {
+			new->o1_type = REGISTER;
+			new->o1 = -o1;
+		} else {
+			err("Destination must be a register!")
+		}
+		if (o2 < 0) {
+			new->o2_type = REGISTER;
+			new->o2 = -o2;
+		} else {
+			new->o2_type = VALUE;
+			new->o2 = o2;
+		}
+		if (o3 < 0) {
+			new->o3_type = REGISTER;
+			new->o3 = -o3;
+		} else {
+			new->o3_type = VALUE;
+			new->o3 = o3;
+		}
+	}
+
+	new->redundant = 0;
+	new->deleted = 0;
+
+	new->last_res = -1;
+	new->is_const = 1;
+	new->first_run = 1;
+}
+
+void storeValues() {
+	for (int i = 0; i < 3; i++) {
+		if (modified[i]) {
+			addISA(STORE, 4 * i, -identifiers[i], 0);
+		}
+	}
+}
+
+#pragma GCC push_options
+#pragma GCC optimize ("O0")
+void printASM(int start) {
+	for (int i = start; i < code_len; i++) {
+		if (code[i].deleted)
+			continue;
+		printISA(&code[i]);
+	}
+}
+
+void printISA(ISA *isa) {
+	const static char insName[][10] = {
+		"UNKNOWN", "add", "sub", "mul", "div", "rem", "UNKNOWN", "UNKNOWN", "UNKNOWN", "UNKNOWN", "UNKNOWN", "UNKNOWN", "UNKNOWN", "UNKNOWN", "UNKNOWN", "UNKNOWN", "load", "store"
+	};
+	const static char format_memory[] = "%s %s %s\n";
+	const static char format_arithmetic[] = "%s %s %s %s\n";
+	const static char format_register[] = "r%u";
+	const static char format_address[] = "[%u]";
+	const static char format_value[] = "%u";
+	static char op1[20], op2[20], op3[20];
+	// if (isa->deleted) {
+	// 	printf("D ");
+	// }
+	if (isa->ins == LOAD) {
+		sprintf(op1, format_register, isa->o1 - 1);
+		sprintf(op2, format_address, isa->o2);
+		printf(format_memory, insName[LOAD], op1, op2);
+	} else if (isa->ins == STORE) {
+		sprintf(op1, format_address, isa->o1);
+		sprintf(op2, format_register, isa->o2 - 1);
+		printf(format_memory, insName[STORE], op1, op2);
+	} else {
+		sprintf(op1, format_register, isa->o1 - 1);
+		if (isa->o2_type == REGISTER)
+			sprintf(op2, format_register, isa->o2 - 1);
+		else
+			sprintf(op2, format_value, isa->o2);
+		if (isa->o3_type == REGISTER)
+			sprintf(op3, format_register, isa->o3 - 1);
+		else
+			sprintf(op3, format_value, isa->o3);
+		
+		printf(format_arithmetic, insName[isa->ins], op1, op2, op3);
+	}
+}
+#pragma GCC pop_options
+
+
+void printRegisters() {
+	for (int i = 1; i <= 256; i++)
+		if (registers[i] != FREE)
+			printf("r%d\t", i - 1);
+	printf("\n");
+	for (int i = 1; i <= 256; i++)
+		if (registers[i] != FREE)
+			printf("%d\t", registers[i]);
+	printf("\n");
 }
 
 void freeAST(AST *now) {
@@ -425,7 +688,7 @@ void token_print(Token *in, size_t len) {
 	};
 	const static char format_str[] = "<Index = %3d>: %-10s, %-6s = %s\n";
 	const static char format_int[] = "<Index = %3d>: %-10s, %-6s = %d\n";
-	for(int i = 0; i < len; i++) {
+	for(int i = 0; i < (int)len; i++) {
 		switch(in[i].kind) {
 			case LPAR:
 			case RPAR:
@@ -500,6 +763,7 @@ void AST_print(AST *head) {
 	AST_print(head->lhs);
 	strcpy(indent_now, "` ");
 	AST_print(head->mid);
+
 	AST_print(head->rhs);
 	indent -= 2;
 	(*indent_now) = '\0';
